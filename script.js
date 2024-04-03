45e7b7e54c68114710e5fcbd8f2c1b569f851126
Anjalivlog <139326744+Anjalivlog@users.noreply.github.com>
2024-04-03 09:45:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 function toCase(text) {
   // write your code here
+	if (str.length === 0) {
+        return '-';
+    }
+    const lowerCase = str.toLowerCase();
+    const upperCase = str.toUpperCase();
+    return lowerCase + '-' + upperCase;
 }
 
 // DO not change the code below
